30fc6b451f3ddb710d0ca610a9d1ae80f378deb0
PLI-C Programming Atharva_RAI <arharvarai5059@gmail.com>
2026-09-01 12:35:57 +0530

    Add initial RaiPL9.C file with main function

---
+++ RaiPL9.C
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    // This is a single-line coment....
+    /*
+    This is a multi-line coment.
+    It can span across multiple lines .
+    */
+  return 0;
+} 
